9f5692cc3f98f59f656abfd56e30fad9efb3caa9
Herdiyan Adam Putra <82978131+herdiyana256@users.noreply.github.com>
2022-10-09 19:53:37 +0700

    React_UI

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ToDo from "./containers/ToDo.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <ToDo />
+    </div>
+  );
+}
+
+export default App;
